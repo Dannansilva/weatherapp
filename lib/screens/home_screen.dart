da08497e7145c70e89c0cc627f54499eb40cc08b
Dannansilva <dannangunathilaka@gmail.com>
2024-07-16 21:13:03 +0530

    ui decorated

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,67 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.dark,
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(40, 1.2 * kToolbarHeight, 40, 20),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              Align(
+                alignment: AlignmentDirectional(8, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.purpleAccent),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(-8, -0.3),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.purple),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0, -1.4),
+                child: Container(
+                  height: 300,
+                  width: 300,
+                  decoration: BoxDecoration(color: Colors.orange),
+                ),
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.transparent),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
